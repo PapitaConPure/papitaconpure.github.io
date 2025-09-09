233b095137d327e10454d15ccb4ee2eb679cfc19
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-09-09 11:15:02 -0300

    decrease banding for background gradient

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -54,7 +54,7 @@ export default async function RootLayout({ children, params }: Readonly<Props>) 
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 			</head>
 			<body className='w-full bg-background pt-[60px] font-default-sans text-foreground antialiased'>
-				<div className='fixed left-0 top-0 -z-50 h-screen w-screen bg-gradient-to-b from-transparent to-accent-main/10 contrast-more:hidden' />
+				<div className='fixed bottom-0 left-0 -z-50 h-screen w-screen bg-gradient-to-t from-accent-main/15 to-background contrast-more:hidden' />
 				<Header lang={lang} />
 				{children}
 				<Footer lang={lang} />
