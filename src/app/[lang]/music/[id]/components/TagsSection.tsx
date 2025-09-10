6beea72f08d62f7cc388e0f26b3c79671315bf2b
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-09-10 15:41:15 -0300

    Extract component

--- src/app/[lang]/music/[id]/components/TagsSection.tsx
+++ src/app/[lang]/music/[id]/components/TagsSection.tsx
@@ -2,24 +2,34 @@ import { SectionAcrossLocales } from '@/types/i18n';
 import { MusicItem } from '@/types/music';
 import React from 'react';
 
+interface TagProps extends React.HTMLAttributes<HTMLSpanElement> {
+	name: string;
+}
+
+function Tag({ name, ...props }: TagProps) {
+	return (
+		<span
+			{...props}
+			className='my-2 rounded-md border border-secondary-700 bg-secondary-800 px-3 py-1 text-sm text-secondary-300'>
+			{name.toUpperCase()}
+		</span>
+	);
+}
+
 interface TagsSectionProps {
-    item: MusicItem;
-    t: SectionAcrossLocales<'Music'>;
+	item: MusicItem;
+	t: SectionAcrossLocales<'Music'>;
 }
 
 function TagsSection({ item, t }: TagsSectionProps) {
-    if(!item.tags || item.tags.length === 0) return;
+	if (!item.tags || item.tags.length === 0) return;
 
 	return (
 		<section>
 			<h2 className='section-h2'>{t.detailTagsTitle}</h2>
 			<div className='mt-2 flex flex-wrap space-x-2'>
 				{item.tags.map((tag, index) => (
-					<span
-						key={index}
-						className='my-2 rounded-md bg-secondary-800 px-3 py-1 text-sm text-secondary-300'>
-						{tag.toUpperCase()}
-					</span>
+					<Tag key={index} name={tag} />
 				))}
 			</div>
 		</section>
