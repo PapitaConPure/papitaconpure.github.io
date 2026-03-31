f15d7435856dc966cb17cdc61a8e1d44cbbc6d8a
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-03-31 13:20:06 -0300

    add AudioPlayer

---
+++ src/components/AudioPlayer.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import getRoot from '@/lib/getroot';
+import {
+	faHeadphonesAlt,
+	faPause,
+	faPlay,
+	faVolumeHigh,
+	faVolumeMute,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { createContext, PropsWithChildren, useContext, useId, useRef, useState } from 'react';
+
+export interface AudioPlayerTrack {
+	url: string;
+	format: string;
+	name: string;
+	external?: boolean;
+}
+
+export interface AudioPlayerTrackState {
+	audioPlayerTrack: AudioPlayerTrack | null;
+	setAudioPlayerTrack: (x: AudioPlayerTrack | null) => void;
+}
+
+const AudioPlayerTrackContext = createContext<AudioPlayerTrackState | null>(null);
+
+export function AudioPlayerTrackProvider({ children }: PropsWithChildren) {
+	const [audioPlayerTrack, setAudioPlayerTrack] = useState<AudioPlayerTrack | null>(null);
+
+	return (
+		<AudioPlayerTrackContext.Provider value={{ audioPlayerTrack, setAudioPlayerTrack }}>
+			{children}
+		</AudioPlayerTrackContext.Provider>
+	);
+}
+
+export function usePlayerTrack() {
+	const context = useContext(AudioPlayerTrackContext);
+	if (context == null)
+		throw new Error('No se encontró el contexto para reproducir pistas de audio.');
+	return context;
+}
+
+export function AudioPlayer() {
+	const [playing, setPlaying] = useState(false);
+	const [muted, setMuted] = useState(false);
+	const playerRef = useRef<HTMLAudioElement | null>(null);
+
+	const togglePlaying = () => {
+		const wasStopped = playerRef.current?.paused || playerRef.current?.ended;
+
+		if (wasStopped) playerRef.current?.play();
+		else playerRef.current?.pause();
+	};
+
+	const toggleMuted = () => {
+		if (playerRef.current == null) return;
+
+		const newMuteState = !playerRef.current?.muted;
+		playerRef.current.muted = newMuteState;
+		setMuted(newMuteState);
+	};
+
+	const checkPlaying = (playing: boolean) => {
+		setPlaying(playing);
+	};
+
+	const id = useId();
+
+	const { audioPlayerTrack } = usePlayerTrack();
+
+	if (!audioPlayerTrack) return;
+
+	return (
+		<div className='group fixed bottom-6 right-2 z-50 flex min-h-12 min-w-12 max-w-[95%] items-center justify-between rounded-full border border-secondary-800 border-opacity-60 bg-background bg-opacity-10 p-2 shadow-none shadow-black outline-none ring-0 ring-primary-main backdrop-blur-md transition-all hover:scale-110 hover:bg-opacity-20 hover:shadow-lg focus:ring-2 active:bg-opacity-30 sm:bottom-20 sm:right-4 md:bottom-20 md:right-8 md:inline-flex md:min-h-9 md:min-w-9 md:rounded-md'>
+			<audio
+				key={audioPlayerTrack.url}
+				id={`audio-player-${id}`}
+				ref={playerRef}
+				controls
+				autoPlay
+				controlsList='noremoteplayback nofullscreen'
+				preload='metadata'
+				className='hidden'
+				onPlay={() => checkPlaying(true)}
+				onPause={() => checkPlaying(false)}>
+				<source
+					src={
+						audioPlayerTrack.external
+							? audioPlayerTrack.url
+							: getRoot(audioPlayerTrack.url)
+					}
+					type={`audio/${audioPlayerTrack.format}`}
+				/>
+				Your browser does not support the «audio» element.
+			</audio>
+			<button
+				onClick={toggleMuted}
+				className='mr-0 aspect-square w-0 max-w-max scale-y-0 justify-center opacity-0 transition-all duration-200 group-hover:mr-4 group-hover:w-[1024px] group-hover:scale-y-100 group-hover:pl-2 group-hover:opacity-100'>
+				{muted ? (
+					<FontAwesomeIcon icon={faVolumeMute} />
+				) : (
+					<FontAwesomeIcon icon={faVolumeHigh} />
+				)}
+			</button>
+			<div
+				className={`${playing ? 'text-primary-200' : ''} mr-0 hidden w-0 max-w-max scale-x-0 cursor-pointer select-none whitespace-nowrap p-0 text-center text-sm text-foreground/80 opacity-0 transition-all duration-200 group-hover:mr-4 group-hover:w-[1024px] group-hover:scale-x-100 group-hover:pl-2 group-hover:opacity-100 md:block`}>
+				{audioPlayerTrack.name}
+			</div>
+			<button
+				onClick={togglePlaying}
+				className='aspect-square w-0 justify-center opacity-0 transition-all after:absolute after:bottom-0 after:right-0 after:h-full after:w-0 group-hover:w-full group-hover:opacity-100 group-hover:after:w-1/2'>
+				{playing ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
+			</button>
+			<button
+				className={`${playing ? 'text-accent-300' : 'text-foreground/30'} absolute inset-0 flex h-full w-full scale-y-100 items-center justify-center opacity-100 transition-all group-hover:w-0 group-hover:scale-y-0 group-hover:opacity-0`}>
+				<div>
+					<FontAwesomeIcon icon={faHeadphonesAlt} />
+				</div>
+			</button>
+		</div>
+	);
+}
