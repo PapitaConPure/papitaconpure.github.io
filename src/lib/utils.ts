7eed4d581eb09bf026ec0f40dcc499f420f7ad05
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-01 00:09:28 -0300

    remove unnecessary deps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
-import { clsx, type ClassValue } from "clsx"
+import clsx from "clsx"
+import { ClassValue } from "class-variance-authority/types"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
