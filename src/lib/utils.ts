3fb5c9c9a033b8593e13c56153165da027f5b93f
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-01 02:46:36 -0300

    Add util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,20 @@
-import clsx from "clsx"
-import { ClassValue } from "class-variance-authority/types"
-import { twMerge } from "tailwind-merge"
+import clsx from 'clsx';
+import { ClassValue } from 'class-variance-authority/types';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function linearToExponential(linear: number): number {
+	return Math.pow(10, (linear - 1) * 2);
+}
+
+export function exponentialToLinear(audioVol: number): number {
+	return Math.log10(audioVol) / 2 + 1;
+}
+
+export function clamp(value: number, min: number, max: number): number {
+	if (min > max) [min, max] = [max, min];
+	return Math.min(Math.max(min, value), max);
 }
