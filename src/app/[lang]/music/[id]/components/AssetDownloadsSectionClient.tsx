114bd79f73362fb35578d713a9a7863af1edf480
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-06-14 00:28:03 -0300

    format

--- src/app/[lang]/music/[id]/components/AssetDownloadsSectionClient.tsx
+++ src/app/[lang]/music/[id]/components/AssetDownloadsSectionClient.tsx
@@ -1,14 +1,9 @@
-"use client";
+'use client';
 
-import {
-	faEye,
-	faFilter,
-	faRefresh,
-	faTableCellsLarge,
-} from "@fortawesome/free-solid-svg-icons";
-import { faTableList } from "@fortawesome/free-solid-svg-icons/faTableList";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type React from "react";
+import { faEye, faFilter, faRefresh, faTableCellsLarge } from '@fortawesome/free-solid-svg-icons';
+import { faTableList } from '@fortawesome/free-solid-svg-icons/faTableList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type React from 'react';
 import {
 	type AnchorHTMLAttributes,
 	type ButtonHTMLAttributes,
@@ -17,42 +12,32 @@ import {
 	useCallback,
 	useEffect,
 	useState,
-} from "react";
+} from 'react';
 import {
 	type AudioPlayerTrack,
 	sendTrackToAudioPlayer,
 	stopAudioPlayer,
 	usePlayerTrack,
-} from "@/components/AudioPlayer";
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover";
-import { assetStylesArray } from "@/data/music";
-import type { Locale } from "@/lib/i18n";
+} from '@/components/AudioPlayer';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { assetStylesArray } from '@/data/music';
+import type { Locale } from '@/lib/i18n';
 import {
 	allAssetFormats,
 	assetFormatKindsIndex,
 	assetKinds,
 	resolveLocalizableField,
-} from "@/lib/music";
-import { simpleHash } from "@/lib/utils";
-import type { SectionComponentProps } from "@/types/i18n";
-import type {
-	AssetFormat,
-	AssetKind,
-	DownloadUrl,
-	MusicItem,
-} from "@/types/music";
+} from '@/lib/music';
+import { simpleHash } from '@/lib/utils';
+import type { SectionComponentProps } from '@/types/i18n';
+import type { AssetFormat, AssetKind, DownloadUrl, MusicItem } from '@/types/music';
 import {
 	AssetBrowserPortal,
 	AssetDownloadCard,
 	AssetDownloadDetail,
-} from "./AssetDownloadsSection";
+} from './AssetDownloadsSection';
 
-interface DirectDownloadButtonProps
-	extends AnchorHTMLAttributes<HTMLAnchorElement> {
+interface DirectDownloadButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
 	downloadStageChildren: ReactNode;
 	idleClassName?: string;
 	downloadingClassName?: string;
@@ -69,10 +54,10 @@ export function DirectDownloadButton({
 	return (
 		// biome-ignore lint/a11y/useValidAnchor: Small cute animation while loading link
 		<a
-			href="#"
+			href='#'
 			{...props}
-			rel="noopener noreferrer"
-			className={`${className} ${downloading ? "pointer-events-none opacity-60" : ""}`}
+			rel='noopener noreferrer'
+			className={`${className} ${downloading ? 'pointer-events-none opacity-60' : ''}`}
 			onClick={() => {
 				setDownloading(true);
 				setTimeout(() => setDownloading(false), 2000);
@@ -85,7 +70,7 @@ export function DirectDownloadButton({
 
 interface AssetDownloadAudioPreviewButtonProps
 	extends ButtonHTMLAttributes<HTMLButtonElement>,
-		SectionComponentProps<"Music"> {
+		SectionComponentProps<'Music'> {
 	download: DownloadUrl;
 	lang: Locale;
 	idleContent: React.ReactNode;
@@ -98,7 +83,7 @@ export function AssetDownloadAudioPreviewButton({
 	t,
 	idleContent,
 	playingContent,
-	className = "",
+	className = '',
 }: AssetDownloadAudioPreviewButtonProps) {
 	const { audioPlayerTrack } = usePlayerTrack();
 	const isPlayingThisTrack = audioPlayerTrack?.url === download.url;
@@ -106,21 +91,19 @@ export function AssetDownloadAudioPreviewButton({
 	const targetTrack: AudioPlayerTrack = {
 		url: download.url,
 		format: download.format,
-		name: `{${download.format.toUpperCase()}} ${download.external ? `[${download.provider}] ` : ""}${resolveLocalizableField(download.label, lang)}`,
+		name: `{${download.format.toUpperCase()}} ${download.external ? `[${download.provider}] ` : ''}${resolveLocalizableField(download.label, lang)}`,
 		external: download.external,
 	};
 
 	return (
 		<button
-			type="button"
+			type='button'
 			onClick={() =>
-				isPlayingThisTrack
-					? stopAudioPlayer()
-					: sendTrackToAudioPlayer(targetTrack)
+				isPlayingThisTrack ? stopAudioPlayer() : sendTrackToAudioPlayer(targetTrack)
 			}
 			aria-label={t.detailDownloadsPreviewAriaLabel}
 			tabIndex={0}
-			className={`flex flex-shrink-0 items-center justify-center rounded-md ${isPlayingThisTrack ? "bg-accent-400" : "bg-secondary-700"} px-5 py-4 text-white transition-colors duration-100 ${isPlayingThisTrack ? "hover:bg-red-600" : "hover:bg-secondary-600"} sm:px-4 sm:py-3 md:px-3 md:py-2 ${className}`}
+			className={`flex flex-shrink-0 items-center justify-center rounded-md ${isPlayingThisTrack ? 'bg-accent-400' : 'bg-secondary-700'} px-5 py-4 text-white transition-colors duration-100 ${isPlayingThisTrack ? 'hover:bg-red-600' : 'hover:bg-secondary-600'} sm:px-4 sm:py-3 md:px-3 md:py-2 ${className}`}
 		>
 			{isPlayingThisTrack ? <>{playingContent}</> : <>{idleContent}</>}
 		</button>
@@ -129,7 +112,7 @@ export function AssetDownloadAudioPreviewButton({
 
 function AssetDownloadsBrowserDisplayIncreaseButton({
 	children,
-	className = "",
+	className = '',
 	...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
 	return (
@@ -139,10 +122,10 @@ function AssetDownloadsBrowserDisplayIncreaseButton({
 		>
 			<FontAwesomeIcon
 				icon={faEye}
-				size="lg"
-				className="text-lg sm:mr-3 md:mr-2 md:text-base"
+				size='lg'
+				className='text-lg sm:mr-3 md:mr-2 md:text-base'
 			/>
-			<span className="flex-grow">{children}</span>
+			<span className='flex-grow'>{children}</span>
 		</button>
 	);
 }
@@ -152,27 +135,16 @@ const MAX_DISPLAYED_ASSETS_DEFAULT = 9;
 const MAX_DISPLAYED_ASSETS_INCREASE = 12;
 
 interface AssetDownloadsBrowserProps
-	extends SectionComponentProps<"Music">,
+	extends SectionComponentProps<'Music'>,
 		HTMLAttributes<HTMLDivElement> {
 	item: MusicItem;
 	lang: Locale;
 }
 
-export function AssetDownloadsBrowser({
-	item,
-	lang,
-	t,
-	...props
-}: AssetDownloadsBrowserProps) {
-	const [activeKindFilters, setActiveKindFilters] = useState(
-		() => new Set<AssetKind>(),
-	);
-	const [activeFormatFilters, setActiveFormatFilters] = useState(
-		() => new Set<AssetFormat>(),
-	);
-	const [maxDisplayedAssets, setMaxDisplayedAssets] = useState(
-		MAX_DISPLAYED_ASSETS_DEFAULT,
-	);
+export function AssetDownloadsBrowser({ item, lang, t, ...props }: AssetDownloadsBrowserProps) {
+	const [activeKindFilters, setActiveKindFilters] = useState(() => new Set<AssetKind>());
+	const [activeFormatFilters, setActiveFormatFilters] = useState(() => new Set<AssetFormat>());
+	const [maxDisplayedAssets, setMaxDisplayedAssets] = useState(MAX_DISPLAYED_ASSETS_DEFAULT);
 	const [detailView, setDetailView] = useState(false);
 
 	const allDownloads = item.downloadUrls;
@@ -251,14 +223,11 @@ export function AssetDownloadsBrowser({
 	const areActiveFormatsWithinActiveKinds = useCallback(() => {
 		for (const activeFormatFilter of activeFormatFilters) {
 			const activeFormatKind = assetFormatKindsIndex[activeFormatFilter];
-			if (activeFormatKind && activeKindFilters.has(activeFormatKind))
-				return true;
+			if (activeFormatKind && activeKindFilters.has(activeFormatKind)) return true;
 		}
 	}, [activeFormatFilters, activeKindFilters]);
 
-	function isDownloadFormatDisplayed(
-		downloadFormat: AssetFormat | null = null,
-	) {
+	function isDownloadFormatDisplayed(downloadFormat: AssetFormat | null = null) {
 		if (activeKindFilters.size === 0) return true;
 		if (downloadFormat == null) return true;
 		if (!areActiveFormatsWithinActiveKinds()) return true;
@@ -299,60 +268,60 @@ export function AssetDownloadsBrowser({
 
 	return (
 		<section>
-			<div className="flex items-center justify-between space-x-2">
-				<h2 className="section-h2">
-					<span>{t.detailDownloadsTitle}</span>{" "}
-					<span className="text-sm font-normal text-foreground/70">
+			<div className='flex items-center justify-between space-x-2'>
+				<h2 className='section-h2'>
+					<span>{t.detailDownloadsTitle}</span>{' '}
+					<span className='text-sm font-normal text-foreground/70'>
 						(
-						{maxDisplayedAssets < allDownloads.length &&
-							maxDisplayedAssets < filteredAssetsCount &&
-							`${maxDisplayedAssets} / `}
+						{maxDisplayedAssets < allDownloads.length
+							&& maxDisplayedAssets < filteredAssetsCount
+							&& `${maxDisplayedAssets} / `}
 						{activeKindFilters.size > 0 && `${filteredAssetsCount} / `}
 						{allDownloads.length})
 					</span>
 				</h2>
-				<div className="flex-grow" />
+				<div className='flex-grow' />
 				<button
-					type="button"
+					type='button'
 					onClick={toggleDetailView}
-					className="group rounded-md p-1.5 outline-none ring-primary-main transition-all hover:bg-secondary-800 focus:ring-2 active:bg-secondary-900"
+					className='group rounded-md p-1.5 outline-none ring-primary-main transition-all hover:bg-secondary-800 focus:ring-2 active:bg-secondary-900'
 				>
 					{detailView ? (
 						<FontAwesomeIcon
 							icon={faTableList}
-							className="aspect-square h-4 w-4 text-foreground/80 transition-all duration-150 group-hover:text-foreground group-focus:text-foreground group-active:text-secondary-300"
-							size="lg"
+							className='aspect-square h-4 w-4 text-foreground/80 transition-all duration-150 group-hover:text-foreground group-focus:text-foreground group-active:text-secondary-300'
+							size='lg'
 						/>
 					) : (
 						<FontAwesomeIcon
 							icon={faTableCellsLarge}
-							className="aspect-square h-4 w-4 text-foreground/80 transition-all duration-150 group-hover:text-foreground group-focus:text-foreground group-active:text-secondary-300"
-							size="lg"
+							className='aspect-square h-4 w-4 text-foreground/80 transition-all duration-150 group-hover:text-foreground group-focus:text-foreground group-active:text-secondary-300'
+							size='lg'
 						/>
 					)}
 				</button>
 				<button
 					onClick={resetAssetBrowser}
-					className="group rounded-md p-1.5 outline-none ring-primary-main transition-all hover:bg-secondary-800 focus:ring-2 active:bg-secondary-900"
+					className='group rounded-md p-1.5 outline-none ring-primary-main transition-all hover:bg-secondary-800 focus:ring-2 active:bg-secondary-900'
 				>
 					<FontAwesomeIcon
 						icon={faRefresh}
-						className="aspect-square h-4 w-4 text-foreground/80 transition-colors duration-150 group-hover:rotate-180 group-hover:text-foreground group-hover:transition-all group-focus:rotate-180 group-focus:text-foreground group-focus:transition-all group-active:text-secondary-300"
-						size="lg"
+						className='aspect-square h-4 w-4 text-foreground/80 transition-colors duration-150 group-hover:rotate-180 group-hover:text-foreground group-hover:transition-all group-focus:rotate-180 group-focus:text-foreground group-focus:transition-all group-active:text-secondary-300'
+						size='lg'
 					/>
 				</button>
 				<Popover>
 					<PopoverTrigger asChild>
-						<button className="group rounded-md p-1.5 outline-none ring-primary-main transition-all hover:bg-secondary-800 focus:ring-2 active:bg-secondary-900">
+						<button className='group rounded-md p-1.5 outline-none ring-primary-main transition-all hover:bg-secondary-800 focus:ring-2 active:bg-secondary-900'>
 							<FontAwesomeIcon
 								icon={faFilter}
-								className="aspect-square h-4 w-4 text-accent-400 transition-all duration-150 group-hover:text-accent-100 group-focus:text-foreground group-active:text-accent-500"
-								size="lg"
+								className='aspect-square h-4 w-4 text-accent-400 transition-all duration-150 group-hover:text-accent-100 group-focus:text-foreground group-active:text-accent-500'
+								size='lg'
 							/>
 						</button>
 					</PopoverTrigger>
-					<PopoverContent className="flex flex-col space-y-6" align="end">
-						<div className="flex justify-center space-x-4">
+					<PopoverContent className='flex flex-col space-y-6' align='end'>
+						<div className='flex justify-center space-x-4'>
 							{assetStylesArray
 								.filter((assetStyle) =>
 									allDownloads.some(
@@ -365,16 +334,16 @@ export function AssetDownloadsBrowser({
 										onClick={() => toggleKindFilter(assetStyle.key)}
 										className={`w-full rounded-md p-4 text-center outline-none ring-primary-main transition-all duration-100 hover:bg-secondary-800 focus-visible:ring-2 active:bg-secondary-700`}
 									>
-										<div className="relative mx-auto h-6 w-6">
+										<div className='relative mx-auto h-6 w-6'>
 											<FontAwesomeIcon
 												icon={assetStyle.icon}
-												className={`absolute inset-0 transition-all duration-75 ${assetStyle.className} ${isDownloadKindDisplayed(assetStyle.key) ? "" : "scale-75 opacity-60"}`}
-												size="lg"
+												className={`absolute inset-0 transition-all duration-75 ${assetStyle.className} ${isDownloadKindDisplayed(assetStyle.key) ? '' : 'scale-75 opacity-60'}`}
+												size='lg'
 											/>
 											<FontAwesomeIcon
 												icon={assetStyle.icon}
-												className={`absolute inset-0 mix-blend-plus-lighter blur-md transition-all duration-200 ${assetStyle.className} ${isDownloadKindDisplayed(assetStyle.key) ? "" : "opacity-0"}`}
-												size="lg"
+												className={`absolute inset-0 mix-blend-plus-lighter blur-md transition-all duration-200 ${assetStyle.className} ${isDownloadKindDisplayed(assetStyle.key) ? '' : 'opacity-0'}`}
+												size='lg'
 											/>
 										</div>
 									</button>
@@ -382,16 +351,16 @@ export function AssetDownloadsBrowser({
 						</div>
 						{activeKindFilters.size > 0 && (
 							<div
-								className={`grid rounded-md bg-background/70 p-2 ${allKinds.size <= 1 ? "grid-cols-1" : allKinds.size === 2 ? "grid-cols-2" : allKinds.size === 3 ? "grid-cols-3" : "grid-cols-4"} gap-2`}
+								className={`grid rounded-md bg-background/70 p-2 ${allKinds.size <= 1 ? 'grid-cols-1' : allKinds.size === 2 ? 'grid-cols-2' : allKinds.size === 3 ? 'grid-cols-3' : 'grid-cols-4'} gap-2`}
 							>
 								{allAssetFormats
 									.filter(
 										(assetFormat) =>
-											assetFormatKindsIndex[assetFormat] &&
-											activeKindFilters.has(
+											assetFormatKindsIndex[assetFormat]
+											&& activeKindFilters.has(
 												assetFormatKindsIndex[assetFormat],
-											) &&
-											allFormats.has(assetFormat),
+											)
+											&& allFormats.has(assetFormat),
 									)
 									.map((assetFormat) => (
 										<button
@@ -400,12 +369,12 @@ export function AssetDownloadsBrowser({
 											className={`relative flex aspect-[3/2] w-full items-center justify-center rounded-md px-1 outline-none ring-primary-main transition-all duration-100 hover:bg-secondary-800 focus-visible:ring-2 active:bg-secondary-700`}
 										>
 											<div
-												className={`absolute rounded-md text-sm font-medium transition-all duration-75 ${isDownloadFormatDisplayed(assetFormat) ? "" : "scale-75 opacity-70"}`}
+												className={`absolute rounded-md text-sm font-medium transition-all duration-75 ${isDownloadFormatDisplayed(assetFormat) ? '' : 'scale-75 opacity-70'}`}
 											>
 												{assetFormat.toUpperCase()}
 											</div>
 											<div
-												className={`absolute rounded-md text-sm font-medium mix-blend-plus-lighter blur-md transition-all duration-75 ${isDownloadFormatDisplayed(assetFormat) ? "" : "opacity-0"}`}
+												className={`absolute rounded-md text-sm font-medium mix-blend-plus-lighter blur-md transition-all duration-75 ${isDownloadFormatDisplayed(assetFormat) ? '' : 'opacity-0'}`}
 											>
 												{assetFormat.toUpperCase()}
 											</div>
@@ -419,9 +388,7 @@ export function AssetDownloadsBrowser({
 			<div {...props}>
 				<AssetBrowserPortal detailView={detailView} t={t}>
 					{allDownloads
-						.filter((download) =>
-							isDownloadDisplayed(download.kind, download.format),
-						)
+						.filter((download) => isDownloadDisplayed(download.kind, download.format))
 						.slice(0, maxDisplayedAssets)
 						.map((download) =>
 							detailView ? (
@@ -442,26 +409,25 @@ export function AssetDownloadsBrowser({
 						)}
 				</AssetBrowserPortal>
 				{maxDisplayedAssets < filteredAssetsCount && (
-					<div className="mt-6 flex flex-col justify-center sm:flex-row sm:space-x-2">
-						{maxDisplayedAssets + MAX_DISPLAYED_ASSETS_INCREASE <=
-							trueMaximum && (
+					<div className='mt-6 flex flex-col justify-center sm:flex-row sm:space-x-2'>
+						{maxDisplayedAssets + MAX_DISPLAYED_ASSETS_INCREASE <= trueMaximum && (
 							<AssetDownloadsBrowserDisplayIncreaseButton
-								id="asset-downloads-show-more"
+								id='asset-downloads-show-more'
 								onClick={showMore}
-								className="bg-primary-main duration-150 hover:bg-primary-700"
+								className='bg-primary-main duration-150 hover:bg-primary-700'
 							>
 								{t.detailDownloadsShowMore}
 							</AssetDownloadsBrowserDisplayIncreaseButton>
 						)}
 						{maxDisplayedAssets < trueMaximum && (
 							<AssetDownloadsBrowserDisplayIncreaseButton
-								id="asset-downloads-show-all"
+								id='asset-downloads-show-all'
 								onClick={showAll}
 								className={
-									maxDisplayedAssets + MAX_DISPLAYED_ASSETS_INCREASE <=
-									trueMaximum
-										? "border border-secondary-800 bg-secondary-900 duration-100 hover:bg-secondary-800"
-										: "bg-primary-main duration-150 hover:bg-primary-700"
+									maxDisplayedAssets + MAX_DISPLAYED_ASSETS_INCREASE
+									<= trueMaximum
+										? 'border border-secondary-800 bg-secondary-900 duration-100 hover:bg-secondary-800'
+										: 'bg-primary-main duration-150 hover:bg-primary-700'
 								}
 							>
 								{t.detailDownloadsShowAll}
