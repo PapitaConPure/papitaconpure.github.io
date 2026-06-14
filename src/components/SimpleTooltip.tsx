b04de98b34a5887700cde365990b94a798559886
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-06-14 01:01:38 -0300

    tweak simple tooltip

--- src/components/SimpleTooltip.tsx
+++ src/components/SimpleTooltip.tsx
@@ -10,12 +10,12 @@ interface SimpleTooltipProps extends React.PropsWithChildren {
 
 function SimpleTooltip({ content, side = 'top', className = '', children }: SimpleTooltipProps) {
 	return (
-		<Tooltip>
+		<Tooltip delayDuration={0}>
 			<TooltipTrigger asChild>{children}</TooltipTrigger>
 			<TooltipContent
 				side={side}
 				className={cn(
-					'min-h-12 min-w-12 border border-secondary-800 border-opacity-60 bg-background bg-opacity-10 p-2 shadow-none shadow-black outline-none ring-0 backdrop-blur-md transition-all sm:bottom-20 sm:right-4 md:bottom-20 md:right-8 md:inline-flex md:min-h-9 md:min-w-9 md:rounded-md',
+					'text-base font-default-sans min-w-12 border border-secondary-800 bg-background bg-opacity-10 px-3 py-2 shadow-none shadow-black outline-none ring-0 backdrop-blur-md transition-all sm:bottom-20 sm:right-4 md:bottom-20 md:right-8 md:inline-flex md:min-h-9 md:min-w-9 md:rounded-md',
 					className,
 				)}
 			>
