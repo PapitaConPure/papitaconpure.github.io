2706712ffb41e33420eef3701eede5a1a25c7967
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-06-14 11:50:07 -0300

    keyboard visuals improvement

--- src/app/[lang]/music/[id]/components/TrackDetailsSectionClient.tsx
+++ src/app/[lang]/music/[id]/components/TrackDetailsSectionClient.tsx
@@ -26,17 +26,17 @@ function LeadDownwardsPanel() {
 		<button
 			type='button'
 			onClick={scrollToVideo}
-			className='group mt-4 hidden items-center justify-center h-full md:flex flex-col'
+			className='group mt-4 hidden items-center justify-center h-full md:flex flex-col outline-none'
 		>
 			<FontAwesomeIcon
 				icon={faCompactDisc}
 				size='10x'
-				className='block text-accent-300 opacity-15 group-hover:animate-spin group-hover:text-accent-main group-hover:opacity-60 ease-out transition-all'
+				className='block text-accent-300 opacity-15 group-hover:animate-spin group-focus-visible:animate-spin group-hover:text-accent-main group-focus-visible:text-accent-main group-hover:opacity-60 group-focus-visible:opacity-60 ease-out transition-all'
 			/>
 			<FontAwesomeIcon
 				icon={faAngleDown}
 				size='6x'
-				className='block text-accent-main scale-y-0 h-0 -rotate-45 overflow-clip opacity-0 group-hover:rotate-0 group-hover:opacity-60 group-hover:scale-y-100 group-hover:h-32 ease-out transition-all'
+				className='block text-accent-main scale-y-0 h-0 -rotate-45 overflow-clip opacity-0 group-hover:rotate-0 group-focus-visible:rotate-0 group-hover:opacity-60 group-focus-visible:opacity-60 group-hover:scale-y-100 group-focus-visible:scale-y-100 group-hover:h-32 group-focus-visible:h-32 ease-out transition-all'
 			/>
 		</button>
 	);
