9efcf0a18eab2cafd7b529ec29af4a0d20397f11
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-06-15 05:47:19 -0300

    Make "Back to Music" keyboard navigable

--- src/components/BackSection.tsx
+++ src/components/BackSection.tsx
@@ -13,14 +13,15 @@ const BackSection = ({ href, children }: BackSectionProps) => {
 		<section className='flex items-center justify-center'>
 			<Link
 				href={href}
-				className='group flex w-full cursor-pointer items-center rounded-md bg-foreground px-6 py-3 text-background transition-all duration-300 hover:pl-3 hover:pr-12 hover:opacity-80 sm:w-3/4 md:w-96'
+				tabIndex={0}
+				className='group flex w-full cursor-pointer items-center rounded-md bg-foreground outline-none ring-0 ring-primary-main px-6 py-3 text-background transition-all duration-300 hover:pl-3 hover:pr-12 hover:opacity-80 focus-visible:pl-3 focus-visible:pr-12 focus-visible:opacity-80 focus-visible:ring-4 sm:w-3/4 md:w-96'
 				aria-label='Volver a la lista de música'>
 				<FontAwesomeIcon
 					icon={faAngleLeft}
 					size='1x'
-					className='max-w-2 flex-shrink-0 cursor-pointer transition-all duration-700 group-hover:mr-4 group-hover:animate-pulse'
+					className='max-w-2 flex-shrink-0 cursor-pointer transition-all duration-700 group-hover:mr-4 group-hover:animate-pulse group-focus-visible:mr-4 group-focus-visible:animate-pulse'
 				/>
-				<div className='flex-grow cursor-pointer select-none text-center transition-all duration-300 group-hover:flex-grow-0'>
+				<div className='flex-grow cursor-pointer select-none text-center transition-all duration-300 group-hover:flex-grow-0 group-focus-visible:flex-grow-0'>
 					{children}
 				</div>
 			</Link>
