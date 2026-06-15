04e62ca9e0abcc4ba4d15e384df4b9fbf1cf6c1d
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-06-15 09:44:47 -0300

    a11y

--- src/app/[lang]/music/[id]/components/TrackDetailsSection.tsx
+++ src/app/[lang]/music/[id]/components/TrackDetailsSection.tsx
@@ -1,4 +1,4 @@
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faCompactDisc, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -82,7 +82,18 @@ function TrackDetailsSection({ lang, item, t }: Readonly<TrackDetailsSectionProp
 						/>
 					</div>
 					{(item.kind === 'album' || item.kind === 'compilation') && (
-						<LeadDownwardsPanel />
+						<LeadDownwardsPanel aria-label={t.detailLeadDownwardsTooltip} className='group'>
+							<FontAwesomeIcon
+								icon={faCompactDisc}
+								size='10x'
+								className='block text-accent-300 opacity-15 group-hover:animate-spin group-focus-visible:animate-spin group-hover:text-accent-main group-focus-visible:text-accent-main group-hover:opacity-60 group-focus-visible:opacity-60 ease-out transition-all'
+							/>
+							<FontAwesomeIcon
+								icon={faAngleDown}
+								size='6x'
+								className='block text-accent-main scale-y-0 h-0 -rotate-45 overflow-clip opacity-0 group-hover:rotate-0 group-focus-visible:rotate-0 group-hover:opacity-60 group-focus-visible:opacity-60 group-hover:scale-y-100 group-focus-visible:scale-y-100 group-hover:h-32 group-focus-visible:h-32 ease-out transition-all'
+							/>
+						</LeadDownwardsPanel>
 					)}
 				</div>
 				<div className='mx-auto flex-grow md:mx-0'>
--- src/app/[lang]/music/[id]/components/TrackDetailsSectionClient.tsx
+++ src/app/[lang]/music/[id]/components/TrackDetailsSectionClient.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { faAngleDown, faCompactDisc } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { type ButtonHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
 
 function scrollToVideo() {
 	const videoPreviewSection = document.getElementById('video-preview');
@@ -21,23 +20,22 @@ function scrollToVideo() {
 	});
 }
 
-function LeadDownwardsPanel() {
+function LeadDownwardsPanel({
+	children,
+	className='',
+	...props
+}: React.PropsWithChildren & ButtonHTMLAttributes<HTMLButtonElement>) {
 	return (
 		<button
 			type='button'
 			onClick={scrollToVideo}
-			className='group mt-4 hidden items-center justify-center h-full md:flex flex-col outline-none'
+			className={cn(
+				'group mt-4 hidden items-center justify-center h-full md:flex flex-col outline-none',
+				className,
+			)}
+			{...props}
 		>
-			<FontAwesomeIcon
-				icon={faCompactDisc}
-				size='10x'
-				className='block text-accent-300 opacity-15 group-hover:animate-spin group-focus-visible:animate-spin group-hover:text-accent-main group-focus-visible:text-accent-main group-hover:opacity-60 group-focus-visible:opacity-60 ease-out transition-all'
-			/>
-			<FontAwesomeIcon
-				icon={faAngleDown}
-				size='6x'
-				className='block text-accent-main scale-y-0 h-0 -rotate-45 overflow-clip opacity-0 group-hover:rotate-0 group-focus-visible:rotate-0 group-hover:opacity-60 group-focus-visible:opacity-60 group-hover:scale-y-100 group-focus-visible:scale-y-100 group-hover:h-32 group-focus-visible:h-32 ease-out transition-all'
-			/>
+			{children}
 		</button>
 	);
 }
